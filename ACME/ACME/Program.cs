6153a6e4dfc50fed128662978446948306913a9e
Andrew-Nally <Andrew_Nally@outlook.com>
2020-05-30 11:35:45 -0500

    ACME PAGE 72 UPDATED

--- ACME/ACME/Program.cs
+++ ACME/ACME/Program.cs
@@ -9,7 +9,12 @@ namespace ACME
     class Program
     {
         static void Main(string[] args)
+
         {
+            Console.WriteLine(" Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember we're accounting on you!");
+            Console.ReadLine();
+
         }
     }
 }
