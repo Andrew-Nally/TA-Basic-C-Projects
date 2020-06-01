94b9a5b624d36a4d8c25a8ab3f9875bdb8f085fb
Andrew-Nally <Andrew_Nally@outlook.com>
2020-06-01 16:04:44 -0500

    Class/Methods

---
+++ ClassMethod/ClassMethod/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethod
+{
+    class Math
+    {
+        public void Multiply(int num1)
+        {
+            int result = num1 * 9;
+            Console.WriteLine("Your number multiplied by 9 equals: " + result);
+        }
+        public void Addition(int num1)
+        {
+            int result = num1 + 9;
+            Console.WriteLine("Your number plus 9 equals: " + result);
+        }
+        public void Divide(int num1)
+        {
+            int result = num1 / 9;
+            Console.WriteLine("Your number divided by 9 equals: " + result);
+        }
+    }
+}
---
+++ ClassMethod/ClassMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a whole number:");
+            math.Multiply(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+
+            math = new Math();
+            Console.WriteLine("Please enter a whole number:");
+            math.Addition(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+
+            math = new Math();
+            Console.WriteLine("Please enter a whole number:");
+            math.Divide(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+
+        }
+
+        
+
+
+
+    }
+}
