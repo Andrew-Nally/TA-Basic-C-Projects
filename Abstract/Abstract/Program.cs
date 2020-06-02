de276c609c27bdf65ba916b6065ec6f01bd3ad6c
Andrew-Nally <Andrew_Nally@outlook.com>
2020-06-02 17:42:23 -0500

    Abstract

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee per = new Employee(); 
+            per.firstName = "Sample";
+            per.lastName = "Student";
+            per.SayName();
+            Console.ReadLine();
+        }
+    }
+}
