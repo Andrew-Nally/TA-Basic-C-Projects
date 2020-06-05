fdce8262c72182b79241189de69e6f9fd7cba5f8
Andrew-Nally <Andrew_Nally@outlook.com>
2020-06-04 21:46:15 -0500

    Log Drill

---
+++ log drill/log drill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number. Any number");
+            string numChoice = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\Andre\Desktop\Tech-Academy-Basic-C-Projects\test.txt", numChoice);
+            string text = File.ReadAllText(@"C:\Users\Andre\Desktop\Tech-Academy-Basic-C-Projects\test.txt");
+            Console.WriteLine("Contents of test.txt = {0}", text);
+            Console.ReadLine();
+        }
+    }
+}
