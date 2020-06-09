22579b2a81ab002368f30eef8be40cf690b4c7cf
Andrew-Nally <Andrew_Nally@outlook.com>
2020-06-09 12:25:45 -0500

    Package Express Drill

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -4,36 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Branching
+namespace shipping
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Package Express. Please Follow The Instructions Below:");
-            Console.ReadLine();
-            Console.WriteLine("Please Enter The Package Weight:");
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please Enter The Package Weight.");
             int weight = Convert.ToInt32(Console.ReadLine());
             if (weight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Please Enter The Package Width:");
-                int width = Convert.ToInt32(Console.ReadLine());
-
                 Console.WriteLine("Please Enter The Package Height:");
                 int height = Convert.ToInt32(Console.ReadLine());
-
+                Console.WriteLine("Please Enter The Package Width:");
+                int width = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Please Enter The Package Length:");
                 int length = Convert.ToInt32(Console.ReadLine());
+                if (height + length + width > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    double quote = Convert.ToDouble((height * length * width) * weight / 100.00);
+                    Console.WriteLine("Your total price is: $" + quote);
+                    Console.ReadLine();
+                }
             }
-            if ((width + height + length) > 50);
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-            }
-            Console.WriteLine("Your estimated total for shipping this package is:  ");
         }
     }
 }
+
+
+
